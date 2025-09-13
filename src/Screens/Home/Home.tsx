b9b7f5a6c0f809d3df32e154ccb8db7af6eb1692
FranciscoVitorAraujo <francisco.vitor@aluno.ufca.edu.br>
2025-09-13 14:35:43 -0300

    Setup inicial do projeto

---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './HomeStyles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <View>  </View> {/* Logo e botão */}
+      <View>  </View> {/* Formulário */}
+    </View>
+  );
+}
---
+++ src/Screens/Home/HomeStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"#fff",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
